e77d0a58cf9f63a89125c165ada61bf68784744b
Joseph Chow <sortofsleepy@gmail.com>
2023-01-18 14:31:07 -0800

    init commit

---
+++ +page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import external_sources from "../external_sources";
+</script>
+
+<section class="intro container mx-auto">
+    <h1 class="lg:text-8xl sm:text-5xl">GraphicallyCreative</h1>
+
+
+    <p class="py-4">
+        Hello! If you've landed here then welcome. This is just a personal site of notes and snippets of things I've come across over the years on a variety of topics
+        mostly related to creative coding, all arranged into one place for easy reference. While mostly for personal use, hopefully it can prove useful to you as well.
+    </p>
+
+    <h1 class="mt-8">
+        Some of the topics will include
+    </h1>
+    <ul>
+        <li>WebGL</li>
+        <li>WebGPU</li>
+        <li>openFrameworks</li>
+        <li>Vulkan</li>
+        <li><a href="/houdini">Houdini</a></li>
+        <li>UnrealEngine</li>
+    </ul>
+</section>
+
+<section class="container mx-auto other-resources">
+
+    <h1 class="mt-8">
+        Other Resources
+    </h1>
+    <ul>
+        {#each external_sources as ext_source}
+            <li><a rel="noreferrer" href="{ext_source.url}" target="_blank">{ext_source.name}</a> </li>
+        {/each}
+    </ul>
+</section>
+
---
+++ houdini/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import HoudiniBlueprint from "$lib/houdini_blueprint.svelte"
+</script>
+
+<section class="container mx-auto">
+
+    <h3>Contents</h3>
+    <ul class="page-list">
+        <li>
+            <a href="#vex-ref">Vex reference</a>
+        </li>
+    </ul>
+</section>
+
+
+<section id="vex-ref" class="container mx-auto mt-8">
+    <h3>Common Vex attribute and type reference</h3>
+    <a href="https://www.johnkunz.com/vex" target="_blank" rel="noreferrer">
+        John Kunz has taken the time to to construct a handy reference guide to common Vex related types and attributes you might run into</a>
+    <p>It contains a number of helpful things like available global variables as well as things like Vellum attributes </p>
+</section>
+
+
+<!--  <a href="#">John Kunz has taken the time to to construct a handy reference guide to common Vex related attributes you might run into</a> -->
